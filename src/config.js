1efe89b0d1d82bf42e618844da5f9e400a13cb49
yopaseopor <yopaseopor@gmail.com>
2022-03-01 00:50:37 +0100

    Update config.js

--- src/config.js
+++ src/config.js
@@ -194,7 +194,7 @@ var config = {
 			group: 'Reciclatge/Reciclaje/Recycling',
 			title: $('#welcome').i18n,
 			query: 'node["recycling:paper"="yes"][!access]({{bbox}});out meta;',
-			iconSrc: imgSrc + 'icones_contenidors/paper.svg',
+			iconSrc: imgSrc + 'icones_contenidors/paper.sv',
 			iconStyle: 'background-color:#FFFFFF',
 			style: function () {
 				var fill = new ol.style.Fill({
