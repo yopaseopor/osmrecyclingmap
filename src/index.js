c91740650272a420484fc0cfc16fe4a160f6e20c
yopaseopor <yopaseopor@gmail.com>
2022-02-28 19:53:02 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -331,7 +331,7 @@ $(function () {
 	// Info Control
 	var infoControlBuild = function () {
 		var container = $('<div>').addClass('ol-control ol-unselectable osmcat-infobutton').html($('<button type="button"><i class="fa fa-info-circle"></i></button>').on('click', function () {
-			window.location.href = 'https://github.com/yopaseopor/osmlitmap';
+			window.location.href = 'https://github.com/yopaseopor/osmrecyclingmap';
 		}));
 		return container[0];
 	};
@@ -342,7 +342,7 @@ $(function () {
 		// Info Control
 	var infoControlBuild2 = function () {
 		var container = $('<div>').addClass('ol-control ol-unselectable osmcat-infobutton2').html($('<button type="button"><i class="fa fa-search-plus"></i></button>').on('click', function () {
-			window.location.href = 'https://mapcomplete.osm.be/index.html?userlayout=https://raw.githubusercontent.com/yopaseopor/osmlitmap/master/src/json/lit.json';
+			window.location.href = 'https://mapcomplete.osm.be/index.html?userlayout=https://raw.githubusercontent.com/yopaseopor/mcquests/master/containeronvas.json';
 		}));
 		return container[0];
 	};
